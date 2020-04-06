e142f178facf9b30988ef12a1920d1e7747ba6fb
Nathália Spinula <n.spinula.costa@avanade.com>
2020-04-06 18:50:11 -0300

    Adicionar tela de perfil, arrumar logout

--- src/components/privateRoute/index.js
+++ src/components/privateRoute/index.js
@@ -4,7 +4,7 @@ import { Redirect, Route } from 'react-router-dom'
 const PrivateRoute = ({ component: Component, ...rest }) => {
 
   // Add your own authentication on the below line.
-  const isLoggedIn = localStorage.getItem('cpf');
+  const isLoggedIn = localStorage.getItem('user');
 
   return (
     <Route

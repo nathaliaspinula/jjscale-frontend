3dcb9ed7d59a44692d915d194364850bf44a06d2
nathaliaspinula <nathaliaspinula@gmail.com>
2020-05-21 21:25:23 -0300

    Refatorar código de Projetos

--- src/pages/project/createProject.js
+++ src/pages/project/createProject.js
@@ -33,10 +33,11 @@ export default class UserForm extends Component{
   }
 
   saveProject = async (e) => {
+    const { nome, apelido, cliente } = this.state;
     api.post('/projeto', {
-      nome: this.state.nome,
-      apelido: this.state.apelido,
-      idcliente: this.state.cliente
+      nome,
+      apelido,
+      idcliente: cliente
     }).then(response => 
       swal("Sucesso!", "Projeto criado.", "success").then(
         this.props.history.push("/project")
@@ -44,68 +45,58 @@ export default class UserForm extends Component{
     ).catch(error => swal("Ocorreu um erro!", "Tente novamente.", "error"));
   }
 
-  handleNomeChange = (e) => {
-    this.setState({ nome: e.target.value });
-  }
-
-  handleApelidoChange = (e) => {
-    this.setState({ apelido: e.target.value });
-  }
-
-  handleClienteChange = (e) => {
-    this.setState({ cliente: e.target.value });
-  }
-
   render() {
     return (
-        <Context container="true">
-              <Typography variant="h6" gutterBottom>
-                  Novo Projeto
-              </Typography>
-              <Grid container spacing={3}>
-                  <Grid item xs={12}>
-                     <TextField
-                        required
-                        id="nome"
-                        name="nome"
-                        label="Nome"
-                        fullWidth
-                        onChange={this.handleNomeChange}
-                      />
-                  </Grid>
-                  <Grid item xs={12}>
-                     <TextField
-                        required
-                        id="apelido"
-                        name="apelido"
-                        label="Apelido"
-                        fullWidth
-                        onChange={this.handleApelidoChange}
-                      />
-                  </Grid>
-                  <Grid item xs={12}>
-                  <InputLabel id="cliente-label">Cliente</InputLabel>
-                  <Select
-                    required
-                    labelId="cliente"
-                    id="cliente"
-                    value={this.state.cliente}
-                    onChange={this.handleClienteChange}
-                    input={<Input />}
-                    fullWidth
-                    >
-                    {this.state.clientes.map(item => 
-                        <MenuItem key={Math.random()} value={item.idcliente}>
-                        {item.razaosocial}
-                        </MenuItem>
-                    )}
-                    </Select>
-                  </Grid>
-                  <Grid item justify="flex-end" container xs={12}>
-                    <Button size="small" variant="contained" onClick={this.saveProject}>Salvar</Button>
-                  </Grid>
-              </Grid>
-        </Context>
+      <Context container="true">
+        <Typography variant="h6" gutterBottom>
+          Novo Projeto
+        </Typography>
+        <Grid container spacing={3}>
+            <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="nome"
+                  name="nome"
+                  label="Nome"
+                  value={this.state.nome}
+                  onChange={(e) => this.setState({nome: e.target.value})}
+                  />
+            </Grid>
+            <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="apelido"
+                  name="apelido"
+                  label="Apelido"
+                  value={this.state.apelido}
+                  onChange={(e) => this.setState({apelido: e.target.value})}
+                />
+            </Grid>
+            <Grid item xs={12}>
+            <InputLabel id="cliente-label">Cliente</InputLabel>
+            <Select
+              required
+              labelId="cliente"
+              id="cliente"
+              value={this.state.cliente}
+              onChange={(e) => this.setState({cliente: e.target.value})}
+              input={<Input />}
+              fullWidth
+              >
+              {this.state.clientes.map(item => 
+                  <MenuItem key={Math.random()} value={item.idcliente}>
+                  {item.razaosocial}
+                  </MenuItem>
+              )}
+              </Select>
+            </Grid>
+            <Grid item justify="flex-end" container xs={12}>
+              <Button size="small" variant="contained" onClick={this.saveProject}>Salvar</Button>
+            </Grid>
+        </Grid>
+      </Context>
     );
   }
 }
